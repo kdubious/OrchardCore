d19939a244073f3f774ff6eb37735a092e9bb3c8
Sebastien Ros <sebastienros@gmail.com>
2021-03-31 16:20:42 -0700

    Detect auto setup

--- src/OrchardCore.Cms.Web/Startup.cs
+++ src/OrchardCore.Cms.Web/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -6,9 +7,21 @@ namespace OrchardCore.Cms.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOrchardCms();
+            var orchard = services.AddOrchardCms();
+
+            if (Configuration.GetValue<bool>("auto-setup"))
+            {
+                orchard.AddSetupFeatures("OrchardCore.AutoSetup");
+            }            
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
